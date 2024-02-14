c7b517062e720e6eede32046672b10886faf54a2
marwahamm04 <112507192+marwahamm04@users.noreply.github.com>
2024-02-14 15:09:45 -0500

    w

--- scriptcos.js
+++ scriptcos.js
@@ -1,18 +1,25 @@
 $(document).ready(function () {
     let clickCounter = 0;
+    let alertShown = false; 
 
     $('#btn-story').click(function () {
         clickCounter++;
         $('#counter').text(clickCounter);
 
         const person = $('#person').val();
-        const fabricMaterial = $('#Fabric_Material').val(); // Changed variable name to fabricMaterial
+        const fabricMaterial = $('#Fabric_Material').val();
         const color = $('#Color').val();
 
-        const story = `${person} customized a ${fabricMaterial} ${color}.`; // Fixed variable name here
+        const story = `${person} customized a ${fabricMaterial} ${color}.`;
         $('#story').text(story);
 
-        let clickMessage = `You have Cuztomized me ${clickCounter} times`;
+        let clickMessage = `Thank you for placing an order! Your order is on the way`;
         $('p').last().text(clickMessage);
+
+       
+        if (!alertShown) {
+            $('.alert').show();
+            alertShown = true;
+        }
     });
 });
